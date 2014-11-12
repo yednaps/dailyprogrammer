418a78c5e6f76b21182b26f9c7acf9c89ff53554
Sanjeev Pandey <yednap@gmail.com>
2014-11-11 22:08:03 -0500

    Fix error in handling test month format

--- easy/188dateParsing/fixDate.py
+++ easy/188dateParsing/fixDate.py
@@ -1,4 +1,16 @@
 def fixDate(inp):
+    mth = {'Apr': '3',
+ 'Aug': '7',
+ 'Dec': '11',
+ 'Feb': '1',
+ 'Jan': '0',
+ 'Jul': '6',
+ 'Jun': '5',
+ 'Mar': '2',
+ 'May': '4',
+ 'Nov': '10',
+ 'Oct': '9',
+ 'Sep': '8'}
     if '/' in inp:
         (m,d,y)=inp.split('/')
     elif '#' in inp:
@@ -8,8 +20,11 @@ def fixDate(inp):
 #    elif ',' in inp and len(inp) == 10:
 #        (m,y,d)=inp.split(' ')
     elif ',' in inp:
-        (m,y,d)=inp.split(' ')
+        (m,d,y)=inp.split(' ')
+        m = mth[m]
         d = d[:-1]
+        if len(m) == 1:
+            m = '0' + m
     else:
         (y,m,d)=inp.split('-')
 
