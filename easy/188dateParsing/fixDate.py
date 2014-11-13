0273dbee1f8ccbbd31aa546101f8797f3e5e2c59
Sanjeev Pandey <yednap@gmail.com>
2014-11-13 06:37:45 -0500

    Make function scriptable

--- easy/188dateParsing/fixDate.py
+++ easy/188dateParsing/fixDate.py
@@ -1,7 +1,13 @@
-def fixDate(inp):
-    m = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'
-    mth = {j:str(i) for i,j in enumerate(' '.split(m))}
+#!/usr/bin/python
 
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit('Usage - fixDate.py filename')
+else:
+    f = sys.argv[1]
+
+def fixDate(inp):
     if '/' in inp:
         (m,d,y)=inp.split('/')
     elif '#' in inp:
@@ -24,3 +30,12 @@ def fixDate(inp):
             y = '20' + y
     
     return '-'.join([y,m,d])
+
+m = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'
+mth = {j:str(i) for i,j in enumerate(m.split(' '))}
+
+f = open(f,'r')
+for line in f:
+    print(fixDate(line.strip()))
+
+
