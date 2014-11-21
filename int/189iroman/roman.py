db03edf9c0b8365411962350650410f51bc8151c
Sanjeev Pandey <yednap@gmail.com>
2014-11-20 22:33:01 -0500

    Add roman numeral validation

--- int/189iroman/roman.py
+++ int/189iroman/roman.py
@@ -3,9 +3,12 @@
 from collections import OrderedDict
 
 roman = OrderedDict([('I',1),('V',5),('X',10),('L',50),('C',100),('D',500),('M',1000)])
+rules = ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']
+bads = [i+j for i in roman.keys() for j in roman.keys() if i+j not in rules and roman[j] > roman[i]]
+bads += ['IIII', 'VVVV', 'XXXX', 'LLLL', 'CCCC', 'DDDD']
 
 def r2d(num):
-	if not set(num).issubset(roman.keys()): return None
+	if not set(num).issubset(roman.keys()) or [i for i in bads if i in num]: return None
 	val = 0
 	for i,d in enumerate(num):
 		if i + 1 < len(num):
@@ -18,5 +21,4 @@ def r2d(num):
 
 def d2r(num):
 	val = ''
-	if 
-	valnum/
+	
