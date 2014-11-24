fbe7e7ad14c2dd3ee469d79ef783513ee7f0839a
Sanjeev Pandey <yednap@gmail.com>
2014-11-23 19:57:38 -0500

    Fix decimal to roman for numbers >= 5000

--- int/189iroman/roman.py
+++ int/189iroman/roman.py
@@ -6,13 +6,16 @@ roman = OrderedDict([('I',1),('V',5),('X',10),('L',50),('C',100),('D',500),('M',
 rules = ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']
 bads = [i+j for i in roman.keys() for j in roman.keys() 
         if i+j not in rules and roman[j] > roman[i]]
-bads += ['IIII', 'VVVV', 'XXXX', 'LLLL', 'CCCC', 'DDDD']
+bads += [i*4 for i in roman.keys()][:-1]
 
 ints = [(1000,'M'),(900,'CM'),(500,'D'),(400,'CD'),(100,'C'),(90,'XC'),
         (50,'L'),(40,'XL'),(10,'X'),(9,'X'),(5,'V'),(4,'IV'),(1,'I')]
 
 def r2d(num):
 	if not set(num).issubset(roman.keys()) or [i for i in bads if i in num]: return None
+	if '(' in num or ')' in num:
+		if '(' in num and ')' in num:
+			return 1000 
 	val = 0
 	for i,d in enumerate(num):
 		if i + 1 < len(num):
@@ -24,10 +27,13 @@ def r2d(num):
 	return val
 
 def d2r(num):
-	val = ''
-	cur = num
-	for i in ints:
-		val += cur/i[0]*i[1]
-		cur %= i[0]
-	return val
+	if num/5000: 
+		return '(' + d2r((num - num%5000)/1000) + ')' + d2r(num%5000)
+	else:
+		val = ''
+		cur = num
+		for i in ints:
+			val += cur/i[0]*i[1]
+			cur %= i[0]
+		return val
 	
